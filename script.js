ec9587fffb6d7492cdc3e08a2a7f4520c828415b
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-02-05 08:44:17 +0500

    Completed

--- script.js
+++ script.js
@@ -7,60 +7,57 @@ let del=document.querySelector(".delete")
 let remover=document.querySelector(".remover")
 
 btnsContainer.addEventListener("click",(e)=>{
+
   for(let i=0;i<characters.length;i++){
     if(e.target == characters[i]  ){
        if(input.value==""  ){
-       alert("please add number first ")
-       return;
-    }
-     if (input.value.includes(characters[i].innerHTML)) {
-          alert("Cannot add consecutive operators");
+          alert("please add a number first ")
+          return;
+       }else if(input.value.endsWith(characters[i].innerHTML) && e.target==characters[i]){
            return;
+       }
+       else{
+          input.value+=characters[i].innerHTML;
+           break;
+       }
      }
-     
-
-     input.value+=characters[i].innerHTML;
-    break;
-    
-    }
-}
-
-for (const element of numbers) {
+   }
+//=========For input numbers===============
+  for (const element of numbers) {
     if(e.target == element){
        input.value+=element.innerHTML
-    }
-}
-
-if(e.target == remover){
+     }
+   }
+//====For AC Button==================
+   if(e.target == remover){
     input.value="";
-}
+   }
 
-if(e.target==del){
+ //===For delete button==========
+  if(e.target==del){
   input.value=input.value.slice(0,-1);
-}
+  
+   }
  
 })
 
-
+//==========For Solution Button===============
 solution.addEventListener("click",()=>{
   if (input.value =="") {
          alert("please enter data for solution")
          return;
   }else{
-      for(let i=0;i<input.value.length;i++){
-        if(input.value[i] ==="+" ||input.value[i] ==="-" || input.value[i]==="*" || input.value[i]==="/"){    
-            let newValue=input.value.split(`${input.value[i]}`);
-            let num1=Number(newValue[0])
-            let num2=Number(newValue[1])
-           if(input.value[i]==="+")  input.value=num1 + num2;
-           else if(input.value[i] ==="-")  input.value=num1 - num2;
-           else if(input.value[i] ==="*")  input.value=num1 * num2;
-           else if(input.value[i] ==="/") input.value=num1 / num2;
-          break;
-        }
-         
-        }
+     
+       input.value=eval(input.value)
     }
 })
+
+//===========For Double Zero===============
+   document.querySelector(".zero").addEventListener("click",()=>{
+     if(input.value==""){
+        return;
+     }
+     input.value+="00";
+   })
 
 
